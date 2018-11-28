55a4ee193c7a0bdce5f92f01096e81da005886cb
kenvinnguyen-terralogic <kenvin.nguyen@terralogic.com>
2018-11-28 16:35:44 +0700

    display internal variables

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Product from './components/Product';
 
 class App extends Component {
   render() {
+      const a = 'Quang';
+      const str = `Hello! I'm ${a}`;
     return (
       <div className="App">
         <Header/>
@@ -17,6 +19,11 @@ class App extends Component {
         <div className="row">
             <Product />
         </div>
+        <br/>
+        <br/>
+        <h1>
+            {str}
+        </h1>
       </div>
     );
   }
