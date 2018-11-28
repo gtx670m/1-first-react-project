51b0e16564864b1febf1fb1974524aba17406a91
kenvinnguyen-terralogic <kenvin.nguyen@terralogic.com>
2018-11-28 16:46:56 +0700

    add a placeholder image

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,7 +5,11 @@ class Product extends Component {
         return (
             <div className="col-sm-6">
                 <div className="card" style={{ width: 400 }}>
-                    <img className="card-img-top" src="img_avatar1.png" alt="Card image" />
+                    <img 
+                        className="card-img-top" 
+                        src="http://www.independentmediators.co.uk/wp-content/uploads/2016/02/placeholder-image.jpg" 
+                        alt="Card image" 
+                    />
                     <div className="card-body">
                         <h4 className="card-title">John Doe</h4>
                         <p className="card-text">Some example text.</p>
