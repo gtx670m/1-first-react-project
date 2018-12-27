f353c796eb491522821e0ef6e07305ab794147b0
kenvinnguyen-terralogic <kenvin.nguyen@terralogic.com>
2018-12-27 14:50:01 +0700

    refs

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+    onAddProduct = () => {
+        alert(this.refs.name.value);
+    }
+    
     render() {
         return (
             <div classname="col-sm-12">
-                <div className="form-group">
-                    <label htmlFor>label</label>
-                    <input type="text" name id className="form-control" placeholder aria-describedby="helpId" />
+                <div className="card bg-primary text-white">
+                    <div className="card-body">
+                        <div className="form-group">
+                            <label htmlFor>label</label>
+                            <input type="text"
+                                name
+                                id
+                                className="form-control"
+                                placeholder
+                                aria-describedby="helpId"
+                                ref="name"
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            class="btn btn-success"
+                            onClick={this.onAddProduct}
+                        >Save</button>
+
+                    </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Save</button>
-            </div>            
+            </div>
         );
     }
 }
